19ec47a52f614c61e5fb285871d657701979664c
gaby06M <gabrielamunozmartinez1@gmail.com>
2024-05-21 11:54:32 -0500

    Programa terminado

--- Datosprueba.cpp
+++
@@ -1,8 +0,0 @@
-
-
-
-
-
-// Este codigo implemneta un menu de agregar buscar y mostrar datos meteorologicos
-// Tambien utiliza un arregl donde se escribe las priemars 10 fechas de los datos meteorologicos
-// y se almacena en un archivo txt 
